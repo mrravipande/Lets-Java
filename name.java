d955457b310627139733407e9a71dd8376451d5f
Ravi Pandey <74792529+mrravipande@users.noreply.github.com>
2022-07-22 10:21:33 +0530

    Create name.java

---
+++ name.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class name
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the name:");
+		String name = sc.next();
+
+		System.out.println("Welcome "+name);
+	}
+}
