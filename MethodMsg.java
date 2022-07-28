6cd4351355338c0532e57c18ad2c3645c174c9ee
Ravi Pandey <74792529+mrravipande@users.noreply.github.com>
2022-07-29 01:53:10 +0530

    Create MethodMsg.java

---
+++ MethodMsg.java
@@ -0,0 +1,11 @@
+public class MethodMsg {
+    public static void main(String[] args) {
+
+        String msg = message();
+        System.out.println(msg);
+    }
+    static String message() {
+        String name = "Ravi sir";
+        return name;
+    }
+}
