64f1ca145e0860b5b252aaa4fece8496eae3edf7
Ravi Pandey <74792529+mrravipande@users.noreply.github.com>
2022-08-04 12:28:17 +0530

    Create SwapNumFunction.java

---
+++ SwapNumFunction.java
@@ -0,0 +1,14 @@
+public class swapFunction {
+    public static void main(String[] args) {
+        swap(10, 20);
+    }
+
+    static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = "+a+" b = "+b);
+
+    }
+}
