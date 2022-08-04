b54f9da0a2e64f1a0a48c78ffd5039cb76f12021
Ravi Pandey <74792529+mrravipande@users.noreply.github.com>
2022-08-04 12:24:21 +0530

    Create FuncArgument.java

---
+++ FuncArgument.java
@@ -0,0 +1,10 @@
+public class FuncArgument {
+    public static void main(String[] args) {
+        System.out.println(Addition(10, 20));
+    }
+    static int Addition(int num1, int num2) {
+        int sum = num1 + num2;
+
+        return sum;
+    }
+}
